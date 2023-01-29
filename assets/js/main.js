b27fea38cbe73963f5060801d0725b0d469b9f05
s00tba11 <104709674+s00tba11@users.noreply.github.com>
2023-01-29 22:33:14 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -194,6 +194,35 @@
     }
   });
 
+/**
+   * Certificates slider
+   */
+new Swiper('.certificates-slider', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 20
+    }
+  }
+});
+
   /**
    * Porfolio isotope and filter
    */
